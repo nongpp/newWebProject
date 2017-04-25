2286d71c07e42aefb47a443729eb676d50984ed8
nongpp <nongnong.pp@gmail.com>
2017-04-26 02:43:31 +0700

    new
    new

--- soft-eng.js
+++ soft-eng.js
@@ -6,13 +6,13 @@ function courseShow(){
 	document.getElementById("general").style.display = 'none';
 	document.getElementById("course").style.display = 'block';
 	document.getElementById("plan").style.display = 'none';
-	document.getElementById("bodyData").style.height = '500px';
+	document.getElementById("bodyData").style.height = '430px';
 }
 function aboutShow(){
 	document.getElementById("general").style.display = 'block';
 	document.getElementById("course").style.display = 'none';
 	document.getElementById("plan").style.display = 'none';
-	document.getElementById("bodyData").style.height = '1600px';
+	document.getElementById("bodyData").style.height = '1100px';
 }
 function planShow(){
 	document.getElementById("plan").style.display = 'block';
